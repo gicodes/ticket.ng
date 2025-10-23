a0c6c1f7a96ec17a7a9ef633264a1de98a50753d
Gideon I <gicodes9@gmail.com>
2025-10-23 18:44:48 +0100

    Refactored Dockerfile to build dist/ before npm run

--- backend/dist/routes/team/route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const api_1 = require("../../controllers/team/invite/api");
-const router = (0, express_1.Router)();
-router.post("/invite", api_1.inviteTeamMember);
-router.post("/accept-invite", api_1.acceptInvite);
-exports.default = router;
--- backend/dist/routes/webhooks/route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const api_1 = require("../../controllers/webhooks/api");
-const router = (0, express_1.Router)();
-router.post("/email", api_1.sendEmailAPI);
-router.post("/slack", api_1.sendSlackAPI);
-router.post("/sms", api_1.sendSMS);
-exports.default = router;
