5a2b3419a998370508227cae87a66b0d4b02e20e
Gideon I <gicodes9@gmail.com>
2025-10-27 18:13:58 +0100

    BE: Transporter verify with enforce secure config

--- backend/src/lib/sendEmail.ts
+++ backend/src/lib/sendEmail.ts
@@ -1,20 +1,26 @@
 import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: 587,
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
-});
-
 type SendEmailProps = {
   from?: string;
   to: string;
   subject: string;
   html: any;
 }
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: true,
+  },
+});
+
+
 
 export async function sendEmail(
   {
@@ -24,6 +30,13 @@ export async function sendEmail(
   }: SendEmailProps
 ) {
   try {
+    await transporter.verify((error, success) => {
+      if (error) {
+        console.error('SMTP connection error:', error);
+      } else {
+        console.log('Server is ready to take messages');
+      }
+    });
     await transporter.sendMail({
       from: `"TicTask - Naija's Number One HR App" <${process.env.MAIL_USER}>`,
       to,
